f4b6a4a53b7da1b1592b027b3af2fee9d7ddcb6a
PalmNeko <nitunamoni13579@gmail.com>
2025-07-03 04:42:08 +0900

    update: document

--- tournament/src/presentation/api/errors/error.ts
+++ tournament/src/presentation/api/errors/error.ts
@@ -1,4 +1,4 @@
 import { createError } from '@fastify/error';
 
 
-export const NotImplementedError = createError('TRT_ERR_NOT_IMPLEMENTED', 'This feature is not implemented.', 500)
+export const NotImplementedError = createError('TRT_ERR_NOT_IMPLEMENTED', 'この機能は実装されていません', 500)
--- tournament/src/presentation/api/openapiConfig.ts
+++ tournament/src/presentation/api/openapiConfig.ts
@@ -9,13 +9,16 @@ docker-compose up -d
 \`\`\`
 で起動できるようにする予定。
 
-### 使い方
+### 使用例
 1. [POST] \`tournaments\`でトーナメントを作成する。
 1. [POST] \`participants\`で参加者を追加する。
 1. [POST] \`tournaments/:id/open\`でトーナメントを開始する。
-1. [GET] \`tournaments/:id\`で適宜トーナメントを取得する。
-1. [POST] \`match/:id/done\`で対戦結果を登録するを進める。
-1. [POST] \`tournament/close\`でトーナメントを閉じる。
+1. [PUT] \`participants/:id/ready\`で参加者をreadyにする。
+1. [PUT] \`participants/:id/cancel\`で参加者をpendingにする。
+1. [PUT] \`participants/:id/battle\`で参加者をin_progressにする。
+1. [POST] \`history\`で対戦結果を保存する。
+1. 参加者が一人になるまで、4, 5, 6を繰り返す。
+1. 一人になると、自動で終了。
 
 ### 共通エラー
 何かしらのエラーが発生した場合以下のエラーが発生する可能性があります。
@@ -43,13 +46,14 @@ docker-compose up -d
 
  以下はこのシステムで更に定義されているものです。
 
- | Code | Description | How to solve |
+ | コード | 説明 | 解決法 |
  | --- | :---: | :---: |
- | TRT_ERR_NOT_IMPLEMENTED | This feature is not implemented | Please contact this server maintainer |
+ | TRT_ERR_NOT_IMPLEMENTED | この機能は実装されていません | 管理者に連絡してください |
 
 ### ドキュメンテーションパス
  * \`/documentation\` : このページ
  * \`/documentation/json\` : このページを生成するための生のopenapi.json
+ * \`/documentation/yaml\` : このページを生成するための生のopenapi.yaml
 
 `
 
