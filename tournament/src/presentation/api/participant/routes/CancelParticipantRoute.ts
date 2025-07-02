da9715ffcc361592d88909cd43bde6cccecd328c
PalmNeko <nitunamoni13579@gmail.com>
2025-07-03 04:30:01 +0900

    add: 準備完了とキャンセル

---
+++ tournament/src/presentation/api/participant/routes/CancelParticipantRoute.ts
@@ -0,0 +1,30 @@
+import { Type } from "@sinclair/typebox";
+import { FastifyInstance } from "fastify";
+import { NotImplementedError } from "../../errors/error";
+import { ParticipantSchema } from "../../schemas/ParticipantSchema";
+import { OKSchema } from "../../schemas/OtherSchema";
+
+const description = `
+# 概要
+参加者のステータスをpendingにします。
+
+# 注意点
+ * stateがreadyの時のみ可能です。
+ * トーナメントがopenの時のみ可能です。
+`
+
+export function CancelParticipantRoute(fastify: FastifyInstance) {
+	fastify.put('/participants/:id/cancel', {
+		schema: {
+			description,
+			tags: ["participant"],
+			summary: "参加者のステータスをpendingにする",
+			params: Type.Pick(ParticipantSchema(), ['id']),
+			response: {
+				200: OKSchema()
+			}
+		}
+	}, () => {
+		throw NotImplementedError();
+	})
+}
--- tournament/src/presentation/api/participant/routes/ParticipantRoutes.ts
+++ tournament/src/presentation/api/participant/routes/ParticipantRoutes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { CreateParticipantRoute } from "./CreateParticipantRoute";
+import { ReadyParticipantRoute } from "./ReadyParticipantRoute";
+import { CancelParticipantRoute } from "./CancelParticipantRoute";
 
 export function ParticipantRoutes(fastify: FastifyInstance) {
 	const routes = [
-		CreateParticipantRoute
+		CreateParticipantRoute,
+		ReadyParticipantRoute,
+		CancelParticipantRoute,
 	] as const;
 
 	routes.forEach((route) => fastify.register(route));
