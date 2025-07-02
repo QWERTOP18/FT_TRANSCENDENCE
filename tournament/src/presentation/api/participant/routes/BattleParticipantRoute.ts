6ae4ee462c16fc14fc4abe980507041844a5b7ca
PalmNeko <nitunamoni13579@gmail.com>
2025-07-03 04:48:42 +0900

    add: battle route

---
+++ tournament/src/presentation/api/participant/routes/BattleParticipantRoute.ts
@@ -0,0 +1,30 @@
+import { Type } from "@sinclair/typebox";
+import { FastifyInstance } from "fastify";
+import { NotImplementedError } from "../../errors/error";
+import { ParticipantIdSchema, ParticipantSchema } from "../../schemas/ParticipantSchema";
+import { OKSchema } from "../../schemas/OtherSchema";
+
+const description = `
+# 概要
+複数の参加者のステータスをin_progressにします。
+
+# 注意点
+ * stateがreadyの時のみ可能です。
+ * トーナメントがopenの時のみ可能です。
+`
+
+export function BattleParticipantRoute(fastify: FastifyInstance) {
+	fastify.put('/participants/battle', {
+		schema: {
+			description,
+			tags: ["participant"],
+			summary: "参加者のステータスをin_progressにする",
+			body: Type.Array(ParticipantIdSchema()),
+			response: {
+				200: OKSchema()
+			}
+		}
+	}, () => {
+		throw NotImplementedError();
+	})
+}
