ab15289d316c89a943e3f93a1fa3842f2731026e
PalmNeko <nitunamoni13579@gmail.com>
2025-07-09 06:53:14 +0900

    update: seed

--- tournament/src/infrastructure/Prisma/prisma/seed.ts
+++ tournament/src/infrastructure/Prisma/prisma/seed.ts
@@ -6,16 +6,24 @@ async function main() {
 
 async function seed() {
 	const prisma = new PrismaClient();
-	const ownerId = crypto.randomUUID();
-	await prisma.tournament.create({
-		data: {
+	const participants = {
+		owner1: { id: '697a1e3f-85a9-47f8-8106-b684328ba106', external_id: "owner-1", state: "pending" }
+	} as const;
+	const tournamentIds = {
+		id1: "df6065be-4e09-4346-99e6-5aa5200cd0c5"
+	}
+	await prisma.tournament.upsert({
+		where: { id: tournamentIds.id1 },
+		update: {},
+		create: {
+			id: tournamentIds.id1,
 			name: "Sample Tournament",
 			description: "This is a sample tournament.",
-			owner_id: ownerId,
+			owner_id: participants.owner1.id,
 			state: "reception",
 			participants: {
 				create: [
-					{ id: ownerId, external_id: "user-1", state: "pending" }
+					participants.owner1
 				]
 			}
 		}
