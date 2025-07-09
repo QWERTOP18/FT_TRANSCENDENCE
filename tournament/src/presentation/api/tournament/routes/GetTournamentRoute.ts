616b7cf5db23197fd8b39be3bdfb18f6bdba5e96
PalmNeko <nitunamoni13579@gmail.com>
2025-07-09 21:24:46 +0900

    add: トーナメント取得

--- tournament/src/presentation/api/tournament/routes/GetTournamentRoute.ts
+++ tournament/src/presentation/api/tournament/routes/GetTournamentRoute.ts
@@ -1,7 +1,14 @@
-import { FastifyInstance } from "fastify";
+import { FastifyError, FastifyInstance } from "fastify";
 import { TournamentSchema } from "../../schemas/TournamentSchema";
-import { NotImplementedError } from "../../errors/error";
+import { NotFoundError, NotImplementedError, SomeError } from "../../errors/error";
 import { Type } from "@sinclair/typebox";
+import { ToStatic } from "../../../../types/ToStatic";
+import { TournamentApplicationService } from "../../../../application/TournamentApplicationServiceFacade";
+import { PrismaRepositoryFactory } from "../../../../infrastructure/Prisma/PrismaReopsitoryFactory";
+import { PrismaClientProvider } from "../../../../infrastructure/Prisma/PrismaClientProvider";
+import { TournamentDTO2JSON } from "../TournamentDTO2JSON";
+import { IsFastifyErrorSchema } from "../IsFastifyErrorSchema";
+import { ErrorSchema } from "../../schemas/ErrorSchema";
 
 const description = `
 # 概要
@@ -10,18 +17,50 @@ const description = `
 # 注意点
 `
 
+const RouteSchema = {
+	Params: Type.Pick(TournamentSchema(), ['id']),
+	Querystring: undefined,
+	Body: undefined,
+	Headers: undefined,
+	Reply: {
+		200: TournamentSchema({ description: "OK" }),
+		404: ErrorSchema({ description: "見つからなかった" })
+	}
+} as const;
+
+type RouteSchemaType = {
+	Params: ToStatic<typeof RouteSchema.Params>,
+	Querystring: ToStatic<typeof RouteSchema.Querystring>,
+	Body: ToStatic<typeof RouteSchema.Body>,
+	Headers: ToStatic<typeof RouteSchema.Headers>,
+	Reply: {
+		200: ToStatic<typeof RouteSchema.Reply[200]>,
+		404: ToStatic<typeof RouteSchema.Reply[404]>
+	};
+}
+
 export function GetTournamentRoute(fastify: FastifyInstance) {
-	fastify.get('/tournaments/:id', {
+	fastify.get<RouteSchemaType>('/tournaments/:id', {
 		schema: {
 			description,
 			tags: ["tournament"],
 			summary: "トーナメント取得",
 			response: {
-				200: TournamentSchema({ description: "OK" })
+				200: RouteSchema.Reply[200],
+				404: RouteSchema.Reply[404],
 			},
-			params: Type.Pick(TournamentSchema(), ['id'])
+			params: RouteSchema.Params
 		}
-	}, () => {
-		throw NotImplementedError();
+	}, async (request, reply) => {
+		const appService = new TournamentApplicationService({
+			repositoryFactory: new PrismaRepositoryFactory(new PrismaClientProvider())
+		});
+
+		const tournament = await appService.getTournament({
+			tournamentId: request.params.id
+		});
+		if (tournament == null)
+			throw new NotFoundError();
+		reply.status(200).send(TournamentDTO2JSON.toJSON(tournament))
 	})
 }
