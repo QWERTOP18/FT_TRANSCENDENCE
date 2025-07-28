517ffdfb03ae1f43c5a87a7944442bd59549a6b4
郭エノク <enoch@kakuenokunoMacBook-Air.local>
2025-07-28 17:34:49 +0900

    json_and_webpack

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,6 +2,6 @@
 export const TOURNAMENT_URL = 'http://localhost:8080';
 
 // gameサービスのベースURL
-export const GAME_URL = 'http://localhost:4000';
+export const GAME_URL = 'http://localhost:8000';
 
 export const SERVERURL = 'http://localhost:8000';
