494ba4ef278bdcacf2f46d9089067ab2261aba6e
PalmNeko <nitunamoni13579@gmail.com>
2025-08-03 11:47:05 +0900

    refactor: auth API

--- cli-user/src/api-wrapper/auth/AuthAPI.ts
+++ cli-user/src/api-wrapper/auth/AuthAPI.ts
@@ -6,48 +6,30 @@ import { AuthError } from "./AuthError";
 export class AuthService {
   async signup(name: string): Promise<User> {
     const requestBody: SignupRequest = { name };
-    return await axios.post<User>(
-      `${config.gatewayURL}auth/signup`,
-      requestBody,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    ).then((response) => {
-      return response.data;
-    })
-      .catch((error) => {
-        if (axios.isAxiosError(error)) {
-          const errorData = error.response?.data as AuthErrorSchema;
-          if (errorData) {
-            throw new AuthError(errorData, `${config.gatewayURL}auth/signup`);
-          }
-        }
-        throw error;
-      });
+    return await this.sendAPIRequest<User>('auth/signup', requestBody, 'POST');
   }
 
   async authenticate(name: string): Promise<User> {
     const requestBody: AuthenticateRequest = { name };
-    return await axios.post<User>(
-      `${config.gatewayURL}auth/authenticate`,
-      requestBody,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    ).then((response) => {
-      return response.data;
+    return await this.sendAPIRequest<User>('auth/authenticate', requestBody, 'POST');
+  }
+
+  async sendAPIRequest<T>(endpoint: string, body?: any, method: 'GET' | 'POST' = 'GET'): Promise<T> {
+    const url = `${config.gatewayURL}${endpoint}`;
+    return await axios({
+      url,
+      method,
+      data: body,
+    }).then((response) => {
+      return response.data as T;
     }).catch((error) => {
       if (axios.isAxiosError(error)) {
         const errorData = error.response?.data as AuthErrorSchema;
         if (errorData) {
-          throw new AuthError(errorData, `${config.gatewayURL}auth/authenticate`);
+          throw new AuthError(errorData, url);
         }
       }
       throw error;
-    })
+    });
   }
 } 
